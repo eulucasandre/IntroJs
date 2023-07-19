b758a43fc50b56fc5420935b9351b6234d869c73
Lucas André <105679596+eulucasandre@users.noreply.github.com>
2023-07-19 17:46:08 -0300

    Add files via upload

---
+++ 2-trabalhandoComVariaveis.js
@@ -0,0 +1,9 @@
+//Console.log("Trabalhando com variáveis");
+console.log("Trabalhando com variáveis");
+//JS é case sensitive (ou seja, não se começa declarando uma variável ou função com letra maíuscula)
+
+const idade = 27;
+const nome = "Lucas"
+
+//não fazer 
+// mes = "Janeiro";
